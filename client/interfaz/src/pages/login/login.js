f2b4b76b4a8fc010b36080ae2560f39c1b4452e4
Noemi <mimi2002@estudiantec.cr>
2022-06-09 23:19:59 -0600

    Funcionamiento de ambos imputs y validación de contraseña

--- client/interfaz/src/pages/login/login.js
+++ client/interfaz/src/pages/login/login.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import './login.css';
 import Title from "./components/Title/Title";
 import Label from "./components/Label/Label";
 import Input from "./components/Input/Input";
 
+
+
 const Login = () => {
+    
+    const[user, setUser] = useState('');
+    const[password, setPassword] = useState('');
+    const[passwordError, setPasswordError] = useState(false);
 
         function handleChange (name,value,)
         {
             if (name==='usuario'){
-                //variable para almacenar
+                setUser(value)
+            }else{
+                if (value.lenght < 6){
+                    setPasswordError(true);
+                }else{
+                    setPasswordError(false);
+                    setPassword(value);
+                }
+                
             }
-        }
+        };
+        console.log('usuario: ', user)
+        console.log('contraseña: ', password)
+
+        function handleSubmit(){
+            let account = {user, password}
+            if(account){
+                console.log('account: ', account)
+            }
+        };
+
 
 
     return(
         <div className='login-container'>
-            <Title text = 'Título'/>
+            <div className='login-content'>
+            <Title text = '¡Bienvenido!'/>
             <Label text = 'Usuario'/>
             <Input
             attribute={{
@@ -29,8 +54,30 @@ const Login = () => {
             handleChange={handleChange}
             />
             <Label text = 'Contraseña'/>
-            {/* <Input/> */}
-        </div>
+            <Input
+            attribute={{
+                id: 'contraseña',
+                name: 'contraseña',
+                type: 'password',
+                placeholder: 'Ingrese su contraseña'
+            }}  
+            handleChange={handleChange}
+            param = {passwordError}
+            />
+
+            {passwordError &&  
+            <label className= 'label-error'>
+                Contraseña inválida o incompleta
+            </label>
+            }
+            <div className='submit-button-container'>
+                <button onClick={handleSubmit} className='submit-button-container'>
+                    Ingresar
+                </button>
+
+            </div>
+        </div>   
+    </div>
     ) 
 };
 
