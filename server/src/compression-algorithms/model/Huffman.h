59c1d91a8079a8cc20fb8688f5f20df4679a3ad0
Carlos12001 <carlos.andres12001@gmail.com>
2022-06-13 11:38:32 -0600

    Huffaman broken

--- server/src/compression-algorithms/model/Huffman.h
+++ server/src/compression-algorithms/model/Huffman.h
@@ -5,9 +5,50 @@
 #ifndef COMPRESSION_ALGORITHMS_HUFFMAN_H
 #define COMPRESSION_ALGORITHMS_HUFFMAN_H
 #include "Compression.h"
+// C++ program to encode and decode a string using
+// Huffman Coding.
+#include <bits/stdc++.h>
+#define MAX_TREE_HT 256
+using namespace std;
+
+// A Huffman tree node
+struct MinHeapNode
+{
+    char data;             // One of the input characters
+    int freq;             // Frequency of the character
+    MinHeapNode *left, *right; // Left and right child
+
+    MinHeapNode(char data, int freq)
+    {
+        left = right = NULL;
+        this->data = data;
+        this->freq = freq;
+    }
+};
+
+// utility function for the priority queue
+struct compare
+{
+    bool operator()(MinHeapNode* l, MinHeapNode* r)
+    {
+        return (l->freq > r->freq);
+    }
+};
 
 class Huffman : public Compression{
+    // to map each character its huffman value
+    map<char, string> codes;
 
+
+// STL priority queue to store heap tree, with respect to their heap root node value
+    priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+
+// to store the frequency of character of the input data
+    map<char, int> freq;
+public:
+    string encode(const string& text) override;
+
+    string decode(const string& text) override;
 };
 
 
