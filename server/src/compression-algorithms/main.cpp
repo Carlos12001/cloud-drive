b8d783a51cc9dcd03cec58b7763a311a4cce54b9
Carlos12001 <carlos.andres12001@gmail.com>
2022-06-13 11:04:41 -0600

    Test algoritms

--- server/src/compression-algorithms/main.cpp
+++ server/src/compression-algorithms/main.cpp
@@ -1,6 +1,38 @@
-#include "controller//CLIProgram.h"
+#include "controller/CLIProgram.h"
+#include "model/LZW.h"
 
 int main(int argc, char* argv[]){
     CLIProgram::start(argc, argv);
+//    string s ("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.");
+//    cout << s << endl;
+//    s = LZW().encode(s);
+//    cout << s << endl;
+//    s = LZW().decode(s);
+//    cout << s << endl;
+//
+//    s = ("Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,");
+//    cout << s << endl;
+//    s = LZW().encode(s);
+//    cout << s << endl;
+//    s = LZW().decode(s);
+//    cout << s << endl;
+//
+//
+//
+//    s = ("cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una");
+//    cout << s << endl;
+//    s = LZW().encode(s);
+//    cout << s << endl;
+//    s = LZW().decode(s);
+//    cout << s << endl;
+////
+////
+////     s = "galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.";
+////    cout << s << endl;
+////    s = LZW().encode(s);
+////    cout << s << endl;
+////    s = LZW().decode(s);
+////    cout << s << endl;
+
     return 0;
 }
