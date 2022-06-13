baa122925febb7e88ee5fc7672b0f86e5a6e7061
Carlos12001 <carlos.andres12001@gmail.com>
2022-06-13 09:07:14 -0600

    LZ78 can used

--- server/src/compression-algorithms/model/LZ78.cpp
+++ server/src/compression-algorithms/model/LZ78.cpp
@@ -3,3 +3,176 @@
 //
 
 #include "LZ78.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+struct Node{
+    int index;
+    string data;
+    Node *next;
+};
+
+
+static void st_Node(Node *head, int index, string data){
+    head->index = index;
+    head->data = data;
+    head->next = NULL;
+}
+
+static void insert_Node(Node *head, int index, string data){
+    Node *new_Node = new Node;
+    new_Node->index = index;
+    new_Node->data = data;
+    new_Node->next = NULL;
+
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        if (curr->next == NULL)
+        {
+            curr->next = new_Node;
+            return;
+        }
+        curr = curr->next;
+    }
+}
+
+static Node *search_Node(Node *head, string data)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        if (data.compare(curr->data) == 0)
+            return curr;
+        else
+            curr = curr->next;
+    }
+    return NULL;
+}
+
+static Node *search_Node(Node *head, int index)
+{
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        if (index == curr->index)
+            return curr;
+        else
+            curr = curr->next;
+    }
+    return NULL;
+}
+
+
+static vector <string> split(string str, char delimiter) {
+    vector<string> internal;
+    stringstream ss(str); // Turn the string into a stream.
+    string tok;
+
+    while (getline(ss, tok, delimiter)) {
+        internal.push_back(tok);
+    }
+
+    return internal;
+}
+
+
+string LZ78::encode(const string &text) {
+    string input = text;
+    Node *dictionary = new Node;
+    string word, result;
+    int length, last_seen, index = 1;
+
+    length = (int)input.length();
+    word = input[0];
+    st_Node(dictionary, 1, word);
+    result += "0," + word;
+
+    for (int i = 1; i < length; i++)
+    {
+        string data;
+        data = input[i];
+
+        re_check:
+        Node *search = search_Node(dictionary, data);
+
+        if (search)
+        {
+            i++;
+            data += input[i];
+            last_seen = search->index;
+            goto re_check;
+        }
+        else
+        {
+            char zero;
+            if (input[i] == ' ')
+                zero = '0';
+            else
+                zero = input[i];
+
+            if ((int)data.length() < 2)
+                result += " " + to_string(0) + "," + zero;
+            else
+                result += " " + to_string(last_seen) + "," + zero;
+
+            index++;
+            if (i != length)
+                insert_Node(dictionary, index, data);
+        }
+    }
+
+    return result;
+}
+
+string LZ78::decode(const string &text) {
+    string input = text;
+    Node *dictionary = new Node;
+    string result;
+
+    vector <string> s_input = split(input, ' ');
+    int zz = 2;
+    int size = s_input.size();
+    for (int i = 0; i < size; i++)
+    {
+        vector <string> ss_input = split(s_input[i], ',');
+
+        if (i == 0)
+        {
+            st_Node(dictionary, 1, ss_input[1]);
+            result += ss_input[1];
+        }
+        else
+        {
+            Node *serched;
+            string get_search = ss_input[1];
+            serched = search_Node(dictionary, stoi(ss_input[0]));
+            if (serched)
+            {
+                result += serched->data + get_search;
+                get_search = serched->data + split(s_input[i], ',')[1];
+                insert_Node(dictionary, zz, get_search);
+            }
+            else
+            {
+                if (stoi(ss_input[0]) == 0)
+                    insert_Node(dictionary, zz, get_search);
+                else
+                    insert_Node(dictionary, zz, get_search);
+
+                result += get_search;
+            }
+            zz++;
+        }
+    }
+
+    if (result[(int)result.length() - 1] == '0')
+        result = result.substr(0, result.size() - 1);
+
+    return result;
+}
--- server/src/compression-algorithms/model/LZ78.h
+++ server/src/compression-algorithms/model/LZ78.h
@@ -8,7 +8,10 @@
 #include "Compression.h"
 
 class LZ78 : public Compression{
+public:
+    string encode(const string& text) override;
 
+    string decode(const string& text) override;
 };
 
 
