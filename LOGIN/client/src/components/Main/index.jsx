11775d040b204862ab3ceacbba85846f7a4f055a
Carlos12001 <carlos.andres12001@gmail.com>
2022-06-17 02:13:47 -0600

    WOrks without stykes

--- LOGIN/client/src/components/Main/index.jsx
+++ LOGIN/client/src/components/Main/index.jsx
@@ -1,9 +1,24 @@
 import styles from "./styles.module.css";
+import React, {useState} from "react";
 
 const Main = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
+	};
+
+
+
+	const [busqueda, setBusqueda]= useState("");
+
+	function comprimirArchivo(){
+		//lo que haga
+	};
+	function descargarArchivo(){
+		//lo que haga
+	};
+	function BuscarArchivo(){
+
 	};
 
 	return (
@@ -14,6 +29,32 @@ const Main = () => {
 					Logout
 				</button>
 			</nav>
+
+			<div className="containerInput">
+				<input
+					className="form-control inputBuscar"
+					value={busqueda}
+					placeholder="Búsqueda por Nombre o Empresa"></input>
+
+
+				<button  onClick={BuscarArchivo} className='btn-success'>
+					Buscar
+				</button>
+			</div>
+
+			<input type="file" className= "input"/>
+
+
+			<div className='compress-button-container'>
+				<button onClick={comprimirArchivo} className='compress-button'>
+					Comprimir archivo
+				</button>
+			</div>
+			<div className='download-button-container'>
+				<button onClick={descargarArchivo} className='download-button'>
+					Descargar archivo
+				</button>
+			</div>
 		</div>
 	);
 };
