f3a1bcf80e4b9f78d977027275be26e760627115
Carlos12001 <carlos.andres12001@gmail.com>
2022-06-18 15:44:06 -0600

    CHange port

--- LOGIN/server/index.js
+++ LOGIN/server/index.js
@@ -19,7 +19,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/serverFiles", serverFiles);
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
 
 const compression = require("./data/compression");
